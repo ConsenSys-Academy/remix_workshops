bed9a248cb174cd37be60027672051d99169c16b
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2019-09-03 12:15:36 -0400

    Update SimpleBank_1_test.sol

--- SimpleBank/Step1/SimpleBank_1_test.sol
+++ SimpleBank/Step1/SimpleBank_1_test.sol
@@ -1,6 +1,5 @@
-
 pragma solidity >=0.4.22 <0.6.0;
-import "remix_tests.sol"; // this import is automatically injected by Remix.
+//import "remix_tests.sol"; // this import is automatically injected by Remix.
 import "./SimpleBank.sol";
 
 contract test3 {
@@ -10,11 +9,12 @@ contract test3 {
        contractToTest = new SimpleBank();
     }
     
-    function testEnrolled () public returns(bool){
-        Assert.equal(contractToTest.enrolled(address(0)), uint(0), "it should be 0");
+    function testEnrolled() public 
+    {
+        require(contractToTest.enrolled(msg.sender) == false, "This account should not be enrolled yet.");
     }
     
     function testOwner () public view returns (bool) {
-        return contractToTest.owner() == msg.sender;
+        return contractToTest.owner() == address(0);
     }
 }
